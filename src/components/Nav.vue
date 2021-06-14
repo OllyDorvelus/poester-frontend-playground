9ac341cab6214fbd16e3e69559c4f23cd47fb310
Olivier Dorvelus <ollydorvelus@gmail.com>
2021-06-13 23:39:16 -0400

    base nav bar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,12 @@
 <template>
   <div id="nav">
     <v-app-bar app>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <div class="d-flex align-center"></div>
+      <v-btn text>
+        <router-link to="/">
+          <span class="mr-2">Poetster Logo</span></router-link
+        >
+      </v-btn>
       <v-spacer></v-spacer>
 
       <v-btn text>
